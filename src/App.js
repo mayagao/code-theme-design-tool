da6617e091232aae3ae233375834f75b7779e03e
Maya Gao <maya@stripe.com>
2017-12-01 15:18:02 -0800

    seperate form container from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-
-import SyntaxColorInputContainer from './components/syntaxColorInputContainer.js'
-import ColorInput from './components/colorInput.js'
-import { darkThemeSyntaxTemplate, darkThemeDerivatives } from './constants/darkThemeTemplate.js'
-import { lightThemeSyntaxTemplate, lightThemeDerivatives } from './constants/lightThemeTemplate.js'
 import CodeContainer from './components/codeContainer'
+import FormContainer from './components/formContainer'
 import ContrastScale from './components/contrastScale'
-
 
 class App extends Component {
   constructor(props) {
@@ -17,22 +12,17 @@ class App extends Component {
     };
   }
   updateBgColor = (value) => {
-    this.setState({ bgColor: value })
+    this.setState({ bgColor: value });
   }
   switchTheme = (theme) => {
     this.setState({ theme: theme, bgColor: theme === 'light' ? '#FBFAF9' :  '#3D2C40'})
   }
+
   render() {
-    const colorInputColumnClasses = 'w-20-l w4 dib mr4-ns mr3 mb3';
- 
     return (
       <div className='ph4 pv5 mw8 center w-100'>
-      <style jsx>{`
-      .CodeMirror { padding: 0px; border-radius: 6px; background: ${this.state.bgColor} !important; }
-      ${this.state.theme === 'light' ? lightThemeDerivatives : darkThemeDerivatives}
-    `}</style>
         <div className='cf mb4 '>
-         <div className='f3 fl'>CodeMirror Theme Maker</div>
+          <div className='f3 fl'>CodeMirror Theme Maker</div>
         </div>
         <CodeContainer
           theme = {this.state.theme}
@@ -40,7 +30,7 @@ class App extends Component {
           snippetIndex={this.state.snippetIndex}
           switchSnippet ={this.switchSnippet}
         />
-        <div className='cf mt3 pv4'>
+        <div className='cf pt4'>
           <div className='w-30-l w-100 fl mb4'>
             <div className='f6 pr4-ns gray pr0 lh-title'>
               Contrast score is calculated based on Web Content Accessibility Guidelines <a 
@@ -53,34 +43,14 @@ class App extends Component {
             </div>
           </div>
           <div className='w-70-l w-100 fl mb4'>
-            <form className='flex flex-wrap'>
-              <div className={`${colorInputColumnClasses}`}>
-              <ColorInput
-                value={this.state.bgColor} 
-                label={'Background'}
-                onChange={this.updateBgColor} />
-              </div>         
-              {this.state.theme === 'light' && lightThemeSyntaxTemplate.map((s, i) => 
-                <div key={i}  className={`${colorInputColumnClasses}`}>
-                  <SyntaxColorInputContainer 
-                    label={s.lable} 
-                    value={s.value} 
-                    background={this.state.bgColor}
-                    className={s.className} 
-                  />
-                </div>)}
-              {this.state.theme === 'dark' && darkThemeSyntaxTemplate.map((s, i) => 
-                <div key={i}  className={`${colorInputColumnClasses}`}>
-                  <SyntaxColorInputContainer 
-                    label={s.lable} 
-                    value={s.value} 
-                    background={this.state.bgColor}
-                    className={s.className} 
-                  />
-                </div>)}
-            </form>
+            <FormContainer 
+              theme = {this.state.theme}
+              bgColor = {this.state.bgColor}
+              updateBgColor = {this.updateBgColor}
+            />
           </div>
         </div>
+
       </div>
     );
   }
